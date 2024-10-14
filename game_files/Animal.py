239eb3efcaaba1fc3aeff49800e954afcb5867fe
hlathery <hlathery@calpoly.edu>
2024-10-14 13:33:55 -0700

    Edited Animal and Person

--- game_files/Animal.py
+++ game_files/Animal.py
@@ -2,17 +2,8 @@ import pygame
 import Util
 
 class Animal(pygame.sprite.Sprite):
-    def __init__(self, game, spritesheet):
+    def __init__(self, game):
         self.game = game
-        self._layer = Util.PERSON_LAYER
-        self.groups = self.game.all_sprites, self.game.animals
-        self.width = Util.TILESIZE/2
-        self.height = Util.TILESIZE/2
-        self.image = spritesheet.get_sprite(0,0,self.width,self.height)
-        self.rect = self.image.get_rect()
-        self.x = 0
-        self.y = 0
-
         self.hunted = False
         self.nourishment = 100
 
--- game_files/Person.py
+++ game_files/Person.py
@@ -2,16 +2,8 @@ import pygame
 import Util
 
 class Person(pygame.sprite.Sprite):
-    def __init__(self, game, x, y, job):
+    def __init__(self, game, job = None):
         self.game = game
-        self._layer = Util.PERSON_LAYER
-        self.groups = self.game.all_sprites, self.game.people
-        self.width = Util.TILESIZE/2
-        self.height = Util.TILESIZE/2
-        self.image = spritesheet.get_sprite(0,0,self.width,self.height)
-        self.rect = self.image.get_rect()
-        self.x = 0
-        self.y = 0
-
+        self.job = job
         self.hunted = False
         self.nourishment = 100
