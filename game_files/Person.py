a24b84321be5b5f7d71452a1e99663e75b75ce29
Adam-Cisneros <111794724+Adam-Cisneros@users.noreply.github.com>
2024-10-25 13:42:17 -0700

    Removed unecessary classes

--- game_files/Person.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import Util
-
-class Person(pygame.sprite.Sprite):
-    def __init__(self, game, job = None):
-        self.game = game
-        self.job = job
-        self.hunted = False
-        self.nourishment = 100
