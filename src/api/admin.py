89d03a976bbed2e3c10c54f35422953be0504961
Hunter Lathery <hlathery@calpoly.edu>
2024-12-04 11:04:06 -0800

    Admin adjusted

--- src/api/admin.py
+++ src/api/admin.py
@@ -33,18 +33,28 @@ def reset():
                     
                     DELETE FROM villagers;
 
+                    INSERT INTO villagers (job_id, age, nourishment)
+                    VALUES
+                        :vil,
+                        :vil,
+                        :vil,
+                        :vil,
+                        :vil;
+
                     UPDATE buildings SET quantity = 0;
 
                     UPDATE buildings SET quantity = 1 WHERE id = 0 OR id = 1
                 """
         # ['resource_name', quantity, building_id] , 0 in this case points to starter town hall
-        default_wood = ['wood', 100, 0] 
-        default_food = ['food', 50, 0]
-        default_water = ['water', 50, 0]
+        default_wood = ['wood', 50, 0] 
+        default_food = ['food', 100, 0]
+        default_water = ['water', 100, 0]
+        default_villager = [0, 18, 100]
 
         connection.execute(sqlalchemy.text(query), {"wood": tuple(default_wood),
                                                     "food": tuple(default_food),
-                                                    "water": tuple(default_water)})
+                                                    "water": tuple(default_water),
+                                                    "vil": tuple(default_villager)})
     
 
     return "Reset Complete"
